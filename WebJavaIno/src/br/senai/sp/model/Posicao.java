e483f3e05d3eeccfb08edb509508733201d09a11
VictorN7 <victorhnogueira@hotmail.com>
2021-11-23 22:46:58 -0300

    Código refatorado e com novas funcionalidades

---
+++ WebJavaIno/src/br/senai/sp/model/Posicao.java
@@ -0,0 +1,18 @@
+package br.senai.sp.model;
+
+public class Posicao {
+
+	public Posicao(int x, int y, int z, int r, int t) {
+		xpos = x;
+		ypos = y;
+		zpos = z;
+		rpos = r;
+		tempo = t;
+	}
+
+	public int xpos = 0;
+	public int ypos = 0;
+	public int zpos = 0;
+	public int rpos = 0;
+	public int tempo = 0;
+}
